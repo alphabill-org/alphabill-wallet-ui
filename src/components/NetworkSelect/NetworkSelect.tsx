6ca78a649e605dc9507bffe913796ffaf9ef58fa
Robert Pihelgas <robert.pihelgas@guardtime.com>
2025-02-03 14:08:17 +0200

    AB-1851 Implement NetworkSelect

---
+++ src/components/NetworkSelect/NetworkSelect.tsx
@@ -0,0 +1,40 @@
+import { ReactElement, useCallback } from 'react';
+
+import { INetwork, useNetwork } from '../../hooks/networkContext';
+import CheckIcon from '../../images/check-ico.svg?react';
+import { SelectBox } from '../SelectBox/SelectBox';
+
+export const NetworkSelect = (): ReactElement => {
+  const { setSelectedNetwork, selectedNetwork, networks } = useNetwork();
+
+  const select = useCallback(
+    (network: INetwork) => {
+      setSelectedNetwork(network);
+    },
+    [setSelectedNetwork],
+  );
+
+  const getOptionKey = useCallback((network: INetwork) => network.id, []);
+
+  const createOption = useCallback(
+    (network: INetwork) => (
+      <>
+        {network.alias} {selectedNetwork?.id === network.id ? <CheckIcon /> : null}
+      </>
+    ),
+    [selectedNetwork],
+  );
+
+  return (
+    <div className="select__options">
+      <SelectBox
+        title="SELECT NETWORK"
+        selectedItem={selectedNetwork?.alias}
+        data={networks}
+        select={select}
+        getOptionKey={getOptionKey}
+        createOption={createOption}
+      />
+    </div>
+  );
+};
