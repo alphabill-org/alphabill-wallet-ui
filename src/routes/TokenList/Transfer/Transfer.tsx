c3a243fffd5a5a1fb56f8b063b6cd4f9d347611d
Mart Mägi <mart.magi@guardtime.com>
2025-02-12 21:12:57 +0200

    Use reset query after transferring token

--- src/routes/TokenList/Transfer/Transfer.tsx
+++ src/routes/TokenList/Transfer/Transfer.tsx
@@ -7,7 +7,6 @@ import { AlwaysTrueProofFactory } from '@alphabill/alphabill-js-sdk/lib/transact
 import { PayToPublicKeyHashProofFactory } from '@alphabill/alphabill-js-sdk/lib/transaction/proofs/PayToPublicKeyHashProofFactory';
 import { UnitId } from '@alphabill/alphabill-js-sdk/lib/UnitId';
 import { Base16Converter } from '@alphabill/alphabill-js-sdk/lib/util/Base16Converter';
-import { useQueryClient } from '@tanstack/react-query';
 import { FormEvent, ReactElement, useCallback } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
@@ -19,6 +18,7 @@ import { TextField } from '../../../components/InputField/TextField';
 import { Loading } from '../../../components/Loading/Loading';
 import { useAlphabill } from '../../../hooks/alphabillContext';
 import { useFungibleTokens } from '../../../hooks/fungibleToken';
+import { Predicates, useResetQuery } from '../../../hooks/resetQuery';
 import { useUnitsList } from '../../../hooks/unitsList';
 import { useVault } from '../../../hooks/vaultContext';
 import BackIcon from '../../../images/back-ico.svg?react';
@@ -29,15 +29,72 @@ export function Transfer(): ReactElement {
   const alphabill = useAlphabill();
   const params = useParams<{ id: string }>();
   const vault = useVault();
-  const queryClient = useQueryClient();
+  const resetQuery = useResetQuery();
   const navigate = useNavigate();
+
+  const transfer = useCallback(async (ev: FormEvent<HTMLFormElement>): Promise<void> => {
+    ev.preventDefault();
+    if (!alphabill) {
+      throw new Error('Invalid alphabill context');
+    }
+    const errors = new Map<FormElements, string>();
+    const data = new FormData(ev.currentTarget);
+    const address = String(data.get('address') ?? '');
+    if (address.length === 0) {
+      errors.set('address', 'Invalid address.');
+    }
+
+    const password = String(data.get('password') ?? '');
+    let signingService;
+    try {
+      signingService = await vault.getSigningService(password, vault.selectedKey!.index);
+    } catch (e) {
+      errors.set('password', e instanceof Error ? e.message : String(e));
+      return;
+    }
+    const proofFactory = new PayToPublicKeyHashProofFactory(signingService);
+
+    const token = fungibleTokens.data?.get(unitId.toString());
+    if (!token) {
+      throw new Error('Token with ID ' + unitId.toString() + 'not found');
+    }
+
+    const feeCreditRecordId = feeCredits.data?.feeCreditRecords.at(0);
+    if (!feeCreditRecordId) {
+      throw new Error('Fee credit not found');
+    }
+    const round = (await alphabill!.tokenClient.getRoundInfo()).roundNumber;
+    const alwaysTrueProofFactory = new AlwaysTrueProofFactory();
+    const newOwnerPredicate = PayToPublicKeyHashPredicate.create(Base16Converter.decode(address));
+    const txo = TransferFungibleToken.create({
+      metadata: {
+        feeCreditRecordId,
+        maxTransactionFee: 10n,
+        referenceNumber: null,
+        timeout: round + 60n,
+      },
+      networkIdentifier: NetworkIdentifier.LOCAL,
+      ownerPredicate: newOwnerPredicate,
+      stateLock: null,
+      stateUnlock: new AlwaysTruePredicate(),
+      token: token,
+      version: 1n,
+    }).sign(proofFactory, proofFactory, [alwaysTrueProofFactory]);
+    const transferHash = await alphabill.tokenClient.sendTransaction(await txo);
+    const transferProof = await alphabill.tokenClient.waitTransactionProof(transferHash, TransferFungibleToken);
+    if (!transferProof.transactionRecord.serverMetadata.successIndicator) {
+      throw new Error('Transfer failed');
+    }
+    await resetQuery.resetUnitList(Predicates.FUNGIBLE_TOKEN);
+    navigate('/units/fungible');
+  }, []);
 
   const fungibleTokens = useFungibleTokens(vault.selectedKey?.publicKey.key ?? null);
   const feeCredits = useUnitsList(vault.selectedKey?.publicKey.key ?? null, PartitionIdentifier.TOKEN);
 
   const unitIdParam = params.id;
   if (!unitIdParam) {
-    throw new Error('');
+    throw new Error('Unit ID is mandatory');
   }
   const unitId = UnitId.fromBytes(Base16Converter.decode(unitIdParam));
 
@@ -62,67 +119,6 @@ export function Transfer(): ReactElement {
       </div>
     );
   }
-
-  const transfer = useCallback(async (ev: FormEvent<HTMLFormElement>): Promise<void> => {
-    ev.preventDefault();
-    const errors = new Map<FormElements, string>();
-    const data = new FormData(ev.currentTarget);
-    const address = String(data.get('address') ?? '');
-    if (address.length === 0) {
-      errors.set('address', 'Invalid address.');
-    }
-
-    const password = String(data.get('password') ?? '');
-    let signingService;
-    try {
-      signingService = await vault.getSigningService(password, vault.selectedKey!.index);
-    } catch (e) {
-      errors.set('password', e instanceof Error ? e.message : String(e));
-      return;
-    }
-    const proofFactory = new PayToPublicKeyHashProofFactory(signingService);
-
-    const round = (await alphabill!.tokenClient.getRoundInfo()).roundNumber;
-    const alwaysTrueProofFactory = new AlwaysTrueProofFactory();
-    const token = fungibleTokens.data?.get(unitId.toString());
-    if (!token) {
-      console.log('Token not found');
-      return;
-    }
-
-    const feeCreditRecordId = feeCredits.data?.feeCreditRecords.at(0);
-    if (!feeCreditRecordId) {
-      console.log('Fee credit not found');
-      return;
-    }
-    const newOwnerPredicate = PayToPublicKeyHashPredicate.create(Base16Converter.decode(address));
-    const txo = TransferFungibleToken.create({
-      metadata: {
-        feeCreditRecordId,
-        maxTransactionFee: 10n,
-        referenceNumber: null,
-        timeout: round + 60n,
-      },
-      networkIdentifier: NetworkIdentifier.LOCAL,
-      ownerPredicate: newOwnerPredicate,
-      stateLock: null,
-      stateUnlock: new AlwaysTruePredicate(),
-      token: token!,
-      version: 1n,
-    }).sign(proofFactory, proofFactory, [alwaysTrueProofFactory]);
-    const transferHash = await alphabill!.tokenClient.sendTransaction(await txo);
-    const transferProof = await alphabill!.tokenClient.waitTransactionProof(transferHash, TransferFungibleToken);
-    if (!transferProof.transactionRecord.serverMetadata.successIndicator) {
-      console.log('Transfer failed');
-      return;
-    }
-    await queryClient.resetQueries({
-      predicate: (query) => {
-        return query.queryKey.at(0) === 'UNITS';
-      },
-    });
-    navigate('/units/fungible');
-  }, []);
 
   return (
     <div className="transfer">
