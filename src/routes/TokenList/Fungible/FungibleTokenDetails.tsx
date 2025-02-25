5f610042520a81d39a5ae9da4dcee78b195b0552
Mart Mägi <mart.magi@outlook.com>
2025-02-25 10:08:20 +0200

    Allow transferring alpha (#30)
    * Allow transferring money partition units
    
    ---------
    
    Co-authored-by: Mart Mägi <mart.magi@guardtime.com>

--- src/routes/TokenList/Fungible/FungibleTokenDetails.tsx
+++ src/routes/TokenList/Fungible/FungibleTokenDetails.tsx
@@ -1,4 +1,3 @@
-import { Bill } from '@alphabill/alphabill-js-sdk/lib/money/Bill';
 import { FungibleToken } from '@alphabill/alphabill-js-sdk/lib/tokens/FungibleToken';
 import { Base16Converter } from '@alphabill/alphabill-js-sdk/lib/util/Base16Converter';
 import { ReactElement } from 'react';
@@ -8,26 +7,13 @@ import { FungibleTokenItem } from './FungibleTokenItem';
 import { ErrorNotification } from '../../../components/ErrorNotification/ErrorNotification';
 import { Loading } from '../../../components/Loading/Loading';
 import { Navbar } from '../../../components/NavBar/NavBar';
-import { ALPHA_KEY } from '../../../constants';
-import { useAggregatedAlphas } from '../../../hooks/aggregatedAlpha';
 import { useAggregatedFungibleTokens } from '../../../hooks/aggregatedFungibleTokens';
-import { useAlphabill } from '../../../hooks/alphabillContext';
 import { useVault } from '../../../hooks/vaultContext';
 
 export function FungibleTokenDetails(): ReactElement {
-  const alphabill = useAlphabill();
   const { selectedKey } = useVault();
   const params = useParams<{ id: string }>();
   const fungibleTokens = useAggregatedFungibleTokens(selectedKey?.publicKey.key ?? null);
-  const alphas = useAggregatedAlphas(selectedKey?.publicKey.key ?? null);
-
-  if (!alphabill) {
-    return (
-      <div className="units--error">
-        <ErrorNotification title="No network selected" info={<Link to="/units/fungible">Go back</Link>} />
-      </div>
-    );
-  }
 
   if (!selectedKey) {
     return (
@@ -37,20 +23,18 @@ export function FungibleTokenDetails(): ReactElement {
     );
   }
 
-  if (fungibleTokens.isPending || alphas.isPending) {
+  if (fungibleTokens.isPending) {
     return <Loading title="Loading..." />;
   }
 
-  if (fungibleTokens.isError || alphas.isError) {
-    const error = fungibleTokens.error || alphas.error;
-
+  if (fungibleTokens.isError) {
     return (
       <div className="units--error">
         <ErrorNotification
           title="Error occurred"
           info={
             <>
-              {error?.message} <br />
+              {fungibleTokens.error.message} <br />
               <Link to="/units/fungible">Go back</Link>
             </>
           }
@@ -59,7 +43,7 @@ export function FungibleTokenDetails(): ReactElement {
     );
   }
 
-  const tokenInfo = params.id === ALPHA_KEY ? alphas.data : fungibleTokens.data?.get(params.id ?? '');
+  const tokenInfo = fungibleTokens.data?.get(params.id ?? '');
   if (!tokenInfo) {
     return (
       <div className="units--error">
@@ -79,7 +63,7 @@ export function FungibleTokenDetails(): ReactElement {
     <div className="units__info">
       <Navbar title={tokenInfo.name} />
       <div className="units__info__content">
-        {tokenInfo.units.map((token: Bill | FungibleToken) => {
+        {tokenInfo.units.map((token: FungibleToken) => {
           return (
             <FungibleTokenItem
               key={Base16Converter.encode(token.unitId.bytes)}
